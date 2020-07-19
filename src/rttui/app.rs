9341506b782168e9cf87616766c037be8efb0158
Jacob Rosenthal <jacobrosenthal@gmail.com>
2020-07-19 09:53:16 -0700

    naive paste chart example

--- src/rttui/app.rs
+++ src/rttui/app.rs
@@ -11,7 +11,8 @@ use tui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
-    widgets::{Block, Borders, List, Paragraph, Tabs, Text},
+    symbols,
+    widgets::{Axis, Block, Borders, Chart, Dataset, List, Paragraph, Tabs, Text},
     Terminal,
 };
 
@@ -128,72 +129,171 @@ impl App {
         let tabs = &self.tabs;
         let current_tab = self.current_tab;
         let mut height = 0;
-        self.terminal
-            .draw(|mut f| {
-                let constraints = if has_down_channel {
-                    &[
-                        Constraint::Length(1),
-                        Constraint::Min(1),
-                        Constraint::Length(1),
-                    ][..]
-                } else {
-                    &[Constraint::Length(1), Constraint::Min(1)][..]
-                };
-                let chunks = Layout::default()
-                    .direction(Direction::Vertical)
-                    .margin(0)
-                    .constraints(constraints)
-                    .split(f.size());
+        if current_tab == 0 {
+            self.terminal
+                .draw(|mut f| {
+                    let constraints = if has_down_channel {
+                        &[
+                            Constraint::Length(1),
+                            Constraint::Min(1),
+                            Constraint::Length(1),
+                        ][..]
+                    } else {
+                        &[Constraint::Length(1), Constraint::Min(1)][..]
+                    };
+                    let chunks = Layout::default()
+                        .direction(Direction::Vertical)
+                        .margin(0)
+                        .constraints(constraints)
+                        .split(f.size());
 
-                let tab_names = tabs.iter().map(|t| t.name()).collect::<Vec<_>>();
-                let tabs = Tabs::default()
-                    .titles(&tab_names.as_slice())
-                    .select(current_tab)
-                    .style(Style::default().fg(Color::Black).bg(Color::Yellow))
-                    .highlight_style(
-                        Style::default()
-                            .fg(Color::Green)
-                            .bg(Color::Yellow)
-                            .modifier(Modifier::BOLD),
-                    );
-                f.render_widget(tabs, chunks[0]);
+                    let tab_names = tabs.iter().map(|t| t.name()).collect::<Vec<_>>();
+                    let tabs = Tabs::default()
+                        .titles(&tab_names.as_slice())
+                        .select(current_tab)
+                        .style(Style::default().fg(Color::Black).bg(Color::Yellow))
+                        .highlight_style(
+                            Style::default()
+                                .fg(Color::Green)
+                                .bg(Color::Yellow)
+                                .modifier(Modifier::BOLD),
+                        );
+                    f.render_widget(tabs, chunks[0]);
 
-                height = chunks[1].height as usize;
+                    height = chunks[1].height as usize;
 
-                // We need to collect to generate message_num :(
-                messages_wrapped = messages
-                    .iter()
-                    .map(|m| wrap_iter(m, chunks[1].width as usize).map(|cow| cow.into_owned()))
-                    .flatten()
-                    .collect();
+                    // We need to collect to generate message_num :(
+                    messages_wrapped = messages
+                        .iter()
+                        .map(|m| wrap_iter(m, chunks[1].width as usize).map(|cow| cow.into_owned()))
+                        .flatten()
+                        .collect();
 
-                let message_num = messages_wrapped.len();
+                    let message_num = messages_wrapped.len();
 
-                let messages: Vec<Text> = messages_wrapped
-                    .iter()
-                    .skip(message_num - (height + scroll_offset).min(message_num))
-                    .take(height)
-                    .map(|m| Text::raw(m))
-                    .collect();
+                    let messages: Vec<Text> = messages_wrapped
+                        .iter()
+                        .skip(message_num - (height + scroll_offset).min(message_num))
+                        .take(height)
+                        .map(|m| Text::raw(m))
+                        .collect();
 
-                let messages = List::new(messages.iter().cloned())
-                    .block(Block::default().borders(Borders::NONE));
-                f.render_widget(messages, chunks[1]);
+                    let messages = List::new(messages.iter().cloned())
+                        .block(Block::default().borders(Borders::NONE));
+                    f.render_widget(messages, chunks[1]);
 
-                if has_down_channel {
-                    let text = [Text::raw(input.clone())];
-                    let input = Paragraph::new(text.iter())
-                        .style(Style::default().fg(Color::Yellow).bg(Color::Blue));
-                    f.render_widget(input, chunks[2]);
-                }
-            })
-            .unwrap();
+                    if has_down_channel {
+                        let text = [Text::raw(input.clone())];
+                        let input = Paragraph::new(text.iter())
+                            .style(Style::default().fg(Color::Yellow).bg(Color::Blue));
+                        f.render_widget(input, chunks[2]);
+                    }
+                })
+                .unwrap();
 
-        let message_num = messages_wrapped.len();
-        let scroll_offset = self.tabs[self.current_tab].scroll_offset();
-        if message_num < height + scroll_offset {
-            self.current_tab_mut()
-                .set_scroll_offset(message_num - height.min(message_num));
+            let message_num = messages_wrapped.len();
+            let scroll_offset = self.tabs[self.current_tab].scroll_offset();
+            if message_num < height + scroll_offset {
+                self.current_tab_mut()
+                    .set_scroll_offset(message_num - height.min(message_num));
+            }
+        } else {
+            self.terminal
+                .draw(|mut f| {
+
+
+                    let constraints = if has_down_channel {
+                        &[
+                            Constraint::Length(1),
+                            Constraint::Min(1),
+                            Constraint::Length(1),
+                        ][..]
+                    } else {
+                        &[Constraint::Length(1), Constraint::Min(1)][..]
+                    };
+                    let chunks = Layout::default()
+                        .direction(Direction::Vertical)
+                        .margin(0)
+                        .constraints(constraints)
+                        .split(f.size());
+
+                    let tab_names = tabs.iter().map(|t| t.name()).collect::<Vec<_>>();
+                    let tabs = Tabs::default()
+                        .titles(&tab_names.as_slice())
+                        .select(current_tab)
+                        .style(Style::default().fg(Color::Black).bg(Color::Yellow))
+                        .highlight_style(
+                            Style::default()
+                                .fg(Color::Green)
+                                .bg(Color::Yellow)
+                                .modifier(Modifier::BOLD),
+                        );
+                    f.render_widget(tabs, chunks[0]);
+                    
+                    let mut signal1 = SinSignal::new(0.2, 3.0, 18.0);
+                    let mut signal2 = SinSignal::new(0.1, 2.0, 10.0);
+                    let data1 = signal1.by_ref().take(200).collect::<Vec<(f64, f64)>>();
+                    let data2 = signal2.by_ref().take(200).collect::<Vec<(f64, f64)>>();
+
+                    let size = f.size();
+                    let chunks = Layout::default()
+                        .direction(Direction::Vertical)
+                        .constraints(
+                            [
+                                Constraint::Ratio(1, 3),
+                                Constraint::Ratio(1, 3),
+                                Constraint::Ratio(1, 3),
+                            ]
+                            .as_ref(),
+                        )
+                        .split(size);
+                    let x_labels = [
+                        format!("{}", 0.0),
+                        format!("{}", (0.0 + 20.0) / 2.0),
+                        format!("{}", 20.0),
+                    ];
+                    let datasets = [
+                        Dataset::default()
+                            .name("data2")
+                            .marker(symbols::Marker::Dot)
+                            .style(Style::default().fg(Color::Cyan))
+                            .data(&data1),
+                        Dataset::default()
+                            .name("data3")
+                            .marker(symbols::Marker::Braille)
+                            .style(Style::default().fg(Color::Yellow))
+                            .data(&data2),
+                    ];
+                    let chart = Chart::default()
+                        .block(
+                            Block::default()
+                                .title("Chart 1")
+                                .title_style(
+                                    Style::default().fg(Color::Cyan).modifier(Modifier::BOLD),
+                                )
+                                .borders(Borders::ALL),
+                        )
+                        .x_axis(
+                            Axis::default()
+                                .title("X Axis")
+                                .style(Style::default().fg(Color::Gray))
+                                .labels_style(Style::default().modifier(Modifier::ITALIC))
+                                .bounds([0.0, 20.0])
+                                .labels(&x_labels),
+                        )
+                        .y_axis(
+                            Axis::default()
+                                .title("Y Axis")
+                                .style(Style::default().fg(Color::Gray))
+                                .labels_style(Style::default().modifier(Modifier::ITALIC))
+                                .bounds([-20.0, 20.0])
+                                .labels(&["-20", "0", "20"]),
+                        )
+                        .datasets(&datasets);
+                    f.render_widget(chart, chunks[1]);
+
+                })
+                .unwrap();
         }
     }
 
@@ -262,4 +362,32 @@ impl App {
 pub fn clean_up_terminal() {
     let _ = disable_raw_mode();
     let _ = execute!(std::io::stdout(), LeaveAlternateScreen, DisableMouseCapture);
+}
+
+#[derive(Clone)]
+pub struct SinSignal {
+    x: f64,
+    interval: f64,
+    period: f64,
+    scale: f64,
+}
+
+impl SinSignal {
+    pub fn new(interval: f64, period: f64, scale: f64) -> SinSignal {
+        SinSignal {
+            x: 0.0,
+            interval,
+            period,
+            scale,
+        }
+    }
+}
+
+impl Iterator for SinSignal {
+    type Item = (f64, f64);
+    fn next(&mut self) -> Option<Self::Item> {
+        let point = (self.x, (self.x * 1.0 / self.period).sin() * self.scale);
+        self.x += self.interval;
+        Some(point)
+    }
 }
